63be6711aef14a3b1ea35a7f04e897f971066370
AndraCristiana07 <taporandra@gmail.com>
2025-12-02 18:09:37 +0000

    added timer for benchmarking guess time

--- commands/guess.js
+++ commands/guess.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { performance } = require('node:perf_hooks');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,9 +11,14 @@ module.exports = {
 					.setRequired(true)
 		),
 	async execute(interaction, games_states) {
+		const startTime = performance.now()
+
 		const country_name = interaction.options.get('country_name').value
 		const user_id = interaction.user.id
 		const guessed = games_states[interaction.guild.id + interaction.channel.id].guess(country_name, user_id, interaction.channel)
+		
+		
+
 		if (guessed) {
 			await interaction.reply(`yay! <@${interaction.user.id}> guessed **"${country_name}"**`);
 		} else {
@@ -22,5 +28,9 @@ module.exports = {
 				}, 1000 * 3)
 			)
 		}
+		const endTime = performance.now()
+		const timeBenchmark = endTime - startTime
+		console.log(`timing benchmark for guess: ${timeBenchmark} ms`)
+
 	},
 };
